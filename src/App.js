b9bbaa72782fc4d993901c7248439a71e508df48
Vanessa Chu <vchu22@users.noreply.github.com>
2020-02-13 23:28:05 -0500

    completed todos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Todo({ todo, index }) {
-    return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo }) {
+    return (
+        <div
+            style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+            className="todo"
+        >
+            {todo.text}
+            <div>
+                <button onClick={() => completeTodo(index)}>Complete</button>
+            </div>
+        </div>
+    );
+}
+
+function TodoForm({ addTodo }) {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue('');
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add todo..."
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
+    );
 }
 
 function App() {
@@ -21,12 +53,30 @@ function App() {
             isCompleted: false,
         },
     ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos);
+    };
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
+    };
+
     return (
         <div className="app">
             <div className="todo-list">
                 {todos.map((todo, index) => (
-                    <Todo key={index} todo={todo} />
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                    />
                 ))}
+                <TodoForm addTodo={addTodo} />
             </div>
         </div>
     );
