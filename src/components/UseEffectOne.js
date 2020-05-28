8f49bd0f841f02803d337e58e82dda0986de5f93
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-05-28 10:22:27 -0400

    Conditionally run effects

--- src/components/UseEffectOne.js
+++ src/components/UseEffectOne.js
@@ -2,14 +2,22 @@ import React, { useState, useEffect } from "react";
 
 function UseEffectOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
+  // pass in a function to be executed after render
   useEffect(() => {
+    console.log("useEffect - Updating document title");
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
       <h3>useEffect 1</h3>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   );
