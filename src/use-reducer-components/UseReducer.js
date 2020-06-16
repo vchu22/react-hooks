46b181fbb1e21b40d1ffa61e719551ca44ed00c5
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-06-16 11:02:01 -0400

    19 - useReducer (simple state & action)

--- src/use-reducer-components/UseReducer.js
+++ src/use-reducer-components/UseReducer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import CounterOne from "./CounterOne";
 
 function UseReducer() {
   // useReducer: a hook for state management, alternative to useState
   // useState is built using useReducer
-  return <div className="new"></div>;
+  return (
+    <div className="new">
+      <CounterOne />
+    </div>
+  );
 }
 
 export default UseReducer;
